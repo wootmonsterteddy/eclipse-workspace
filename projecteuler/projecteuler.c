5925f0e719188c4524f164c0611a67aa48ee242b
wootmonsterteddy <32763526+wootmonsterteddy@users.noreply.github.com>
2017-11-22 11:01:42 +0100

    a

--- projecteuler/projecteuler.c
+++ projecteuler/projecteuler.c
@@ -79,7 +79,7 @@ int main(void)
 Largest prime factor
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
-*/
+ */
 
 unsigned long long sum(const unsigned long long *x)
 {
@@ -94,7 +94,7 @@ unsigned long long sum(const unsigned long long *x)
 
 int main(void)
 {
-	unsigned long long factors[1000] = {0}, i = 2, number = 9009;
+	unsigned long long factors[1000] = {0}, i = 2, number = 600851475143;
 	unsigned int counter = 0;
 
 	while(sum(factors) < number)
@@ -126,7 +126,7 @@ int main(void)
 Largest palindrome product
 A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 Find the largest palindrome made from the product of two 3-digit numbers.
-*/
+ */
 bool isPalindrome(const unsigned long original)
 {
 	bool result = 0;
@@ -188,7 +188,7 @@ int main(void)
 Smallest multiple
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-*/
+ */
 
 int main(void)
 {
@@ -231,7 +231,7 @@ The square of the sum of the first ten natural numbers is,
 Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
 
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-*/
+ */
 
 int main(void)
 {
@@ -256,7 +256,7 @@ int main(void)
 10001st prime
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 What is the 10 001st prime number?
-*/
+ */
 
 int main(void)
 {
@@ -264,7 +264,6 @@ int main(void)
 	unsigned long primes[10001] = {0}, i = 4;
 	primes[0] = 2;
 	primes[1] = 3;
-	printf("Test.\n");
 	while(counter < 10001)
 	{
 		for(unsigned long j = 2; j <= (unsigned long)sqrt(i); j++)
@@ -293,7 +292,7 @@ Largest product in a series
 The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
 (se https://projecteuler.net/problem=8)
 Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
-*/
+ */
 
 int main(void)
 {
@@ -338,7 +337,7 @@ int main(void)
 }
 #endif
 
-#if 1
+#if 0
 /*Problem 9
 Special Pythagorean triplet
 
@@ -353,7 +352,102 @@ Find the product abc.
 
 int main(void)
 {
+	float a = 0, b = 0, c = 0;
+
+	while(a + b + sqrt(a*a +  b*b) != 1000)
+	{
+		a++;
+		b = a + 1;
+		while(a + b + sqrt(a*a +  b*b) < 1000)
+		{
+			b++;
+		}
+	}
+	c = sqrt(a*a + b*b);
+	printf("a = %G, b = %G, c = %G\na + b + c = %G\na*b*c = %f\n",a,b,c,a+b+c,a*b*c);
+	getchar();
 
 	return 0;
 }
+#endif
+
+#if 0
+/*Problem 10
+Summation of primes
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.
+*/
+bool isPrime(const unsigned long long i)
+{
+	bool r = 0;
+	if(i == 2)
+	{
+		return 1;
+	}
+	else if(i == 3)
+	{
+		return 1;
+	}
+	for(unsigned long long j = 2; j <= (unsigned long long)sqrt(i); j++)
+	{
+		if(i%j == 0)
+		{
+			break;
+		}
+		else if(j == (unsigned long long)sqrt(i))
+		{
+			r = 1;
+		}
+	}
+	return r;
+}
+
+int main(void)
+{
+	unsigned long long sum = 0, i = 0;
+
+	while(i < 2000000)
+	{
+		if(isPrime(i) == 1)
+		{
+			sum += i;
+		}
+		i++;
+	}
+
+	printf("%llu",sum);
+
+	return 0;
+}
+#endif
+
+#if 1
+/*Problem 11
+Largest product in a grid
+In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+
+08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+32 98 81 28 64 23 67 10 (26) 38 40 67 59 54 70 66 18 38 64 70
+67 26 20 68 02 62 12 20 95 (63) 94 39 63 08 40 91 66 49 94 21
+24 55 58 05 66 73 99 26 97 17 (78) 78 96 83 14 88 34 89 63 72
+21 36 23 09 75 00 76 44 20 45 35 (14) 00 61 33 97 34 31 33 95
+78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+
+The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+*/
 #endif
